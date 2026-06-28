5a20fcf904e481f0c1138d53ca546a787e9aab39
dsnovojil <novojilovd2@rambler.ru>
2026-06-28 15:01:38 +0300

    Simple CRUD and creation result JSON file

--- phase2_project/main.py
+++ phase2_project/main.py
@@ -1,55 +1,59 @@
 import json
 import datetime
 import sys
+from json import JSONDecodeError
 
 try:
-    db = json.load(open('db.json'))
-except FileNotFoundError:
+    db = json.loads(open('db.json').read())
+    print(db)
+except JSONDecodeError:
     print('Database not found, try restore from backup...')
     try:
         db_backup = json.load(open('db_backup.json'))
         db = db_backup
     except FileNotFoundError:
         print('backup file not found, create new db...')
-        db = {}
+        db = {'task_database': {}}
 
 """
 db stucture
 {
-    "user": {
-            task_id: {
+    "task_database": {
+            "user": str {
+                task_id: str {
                         descr: strint
                         complete: bool
                         date: datetime
-                     }
+                        }
+                    }
             }
 """
 
 def create_task(user: str):
-    task_id = max(db[user].keys()) + 1
+    task_id = int(max(db['task_database'][user].keys())) + 1
     descr = input('Введите текст задачи')
-    db[user][task_id] = {
+    db['task_database'][user][task_id] = {
                          'descr': descr,
                          'complete': False,
                          'date': str(datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S'))
                          }
 
 def read_task(user: str):
-    for user_tasks in db[user].keys():
-        print(db[user][user_tasks])
+    for user_tasks in db['task_database'][user].keys():
+        print(db['task_database'][user][user_tasks])
 
 def delete_task(user: str):
-    task_id = int(input('Введите номер задачи\n'))
-    del db[user][task_id]
+    task_id = input('Введите номер задачи\n')
+    del db['task_database'][user][task_id]
 
 def update_task(user: str):
-    task_id = int(input('Введите номер задачи\n'))
+    task_id = input('Введите номер задачи\n')
     descr = input('Введите новый текст задачи\n')
-    db[user][task_id]['descr'] = descr
+    db['task_database'][user][task_id]['descr'] = descr
 
 def complete_task(user: str):
-    task_id = int(input('Введите номер задачи\n'))
-    db[user][task_id]['complete'] = True
+    task_id = input('Введите номер задачи\n')
+    db['task_database'][user][task_id]['complete'] = True
 
 def main():
     try:
@@ -58,8 +62,8 @@ def main():
         print('Что-то пошло не так')
         sys.exit(0)
 
-    if db.get(user) is None:
-        db[user] = {0:
+    if db['task_database'].get(user) is None:
+        db['task_database'][user] = {0:
                         {
                          'descr': 'Задача для примера',
                          'complete': False,
@@ -74,5 +78,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-    print(db)
-    json.dump(db, open('db.json', 'w'))
+    json.dump(db, open('db.json', 'w'), indent=2)
