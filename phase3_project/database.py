257c3cc4aa4a0d0b839ddfaa62e7574b48c88f33
dsnovojil <novojilovd2@rambler.ru>
2026-07-12 19:15:24 +0300

    Создание отделной таблицы для автора, корректировки полей таблиц для книг, добавления ключа автора в таблицу books

--- phase3_project/database.py
+++ phase3_project/database.py
@@ -2,32 +2,39 @@ import sqlite3
 from sqlite3 import Cursor
 
 def db_create(db_cursor: Cursor):
+    db_cursor.execute('''CREATE TABLE IF NOT EXISTS author(
+                        author_id INTEGER PRIMARY KEY,
+                        name text NOT NULL
+                        )''')
     db_cursor.execute('''CREATE TABLE IF NOT EXISTS books(
                         book_id INTEGER PRIMARY KEY,
-                        book_type int NOT NULL
+                        author_id INTEGER NOT NULL,
+                        book_type int NOT NULL,
+                        FOREIGN KEY (author_id) REFERENCES author(author_id) ON DELETE CASCADE ON UPDATE CASCADE
                         )''')
     db_cursor.execute('''CREATE TABLE IF NOT EXISTS physical_books(
                         book_id INTEGER NOT NULL,
                         name text NOT NULL,
                         pages int NOT NULL,
-                        author text NOT NULL,
                         ISBN text NOT NULL,
                         genre text NOT NULL,
                         publisher text NOT NULL,
                         weight real NOT NULL,
                         cover text NOT NULL,
-                         FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE ON UPDATE CASCADE)''')
+                         FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE ON UPDATE CASCADE
+                        )''')
     db_cursor.execute('''CREATE TABLE IF NOT EXISTS digital_books(
                         book_id INTEGER NOT NULL,
                         name text NOT NULL,
                         pages int NOT NULL,
-                        author text NOT NULL,
                         ISBN text NOT NULL,
                         genre text NOT NULL,
                         publisher text NOT NULL,
                         size real NOT NULL,
                         file_path text NOT NULL,
-                         FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE ON UPDATE CASCADE)''')
+                         FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE ON UPDATE CASCADE
+                        )''')
+
 def db_init() -> Cursor:
     conn = sqlite3.connect('database.db')
     db_cursor = conn.cursor()
@@ -47,17 +54,15 @@ def db_insert(entity_type: int, db_cursor: Cursor, **kwargs) -> None:
             db_cursor.execute('''INSERT INTO physical_books(book_id, 
                                                                 name, 
                                                                 pages, 
-                                                                author, 
                                                                 ISBN, 
                                                                 genre, 
                                                                 publisher, 
                                                                 weigth, 
                                                                 cover) 
-                                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                                 VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
                               (book_id,
                                          kwargs['name'],
                                          kwargs['pages'],
-                                         kwargs['author'],
                                          kwargs['ISBN'],
                                          kwargs['genre'],
                                          kwargs['publisher'],
@@ -67,17 +72,15 @@ def db_insert(entity_type: int, db_cursor: Cursor, **kwargs) -> None:
             db_cursor.execute('''INSERT INTO digital_books(book_id, 
                                                                 name, 
                                                                 pages, 
-                                                                author, 
                                                                 ISBN, 
                                                                 genre, 
                                                                 publisher, 
                                                                 size, 
                                                                 file_path) 
-                                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                                 VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
                               (book_id,
                                          kwargs['name'],
                                          kwargs['pages'],
-                                         kwargs['author'],
                                          kwargs['ISBN'],
                                          kwargs['genre'],
                                          kwargs['publisher'],
@@ -87,4 +90,3 @@ def db_insert(entity_type: int, db_cursor: Cursor, **kwargs) -> None:
             print("Что-то пошло не так")
 if __name__ == '__main__':
     db_cursor = db_init()
-    db_create(db_cursor)
